ad427bbab80718ea2b7a096e3dfed0c92c119d1e
shivamvidhi <shivamayank00@hmail.com>
2020-02-17 17:45:04 +0530

    seventh question added

---
+++ java1/question7/ans_7.java
@@ -0,0 +1,25 @@
+package java1.question7;
+
+public class ans_7 {
+    static{
+        String FName="Shivam";
+
+
+        System.out.println(FName);
+
+
+    }
+    static int age =21;
+    public static void printLNAme(String name)
+    {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        ans_7 a=new ans_7();
+        a.printLNAme("Arora");
+        System.out.println(ans_7.age);
+    }
+
+
+}
