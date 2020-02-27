c45769dc9ec15e99ad37ec5414ab9c0b9b8fe70d
shivamvidhi <49386779+shivamvidhi@users.noreply.github.com>
2020-02-27 22:09:13 +0530

    Update even.java

--- src/question_9/even.java
+++ src/question_9/even.java
@@ -5,5 +5,6 @@ import java.util.stream.Stream;
 
 public class even {
     public static void main(String[] args) {
+        System.out.println(Arrays.asList(1,2,3,4,5,6,7,8,9).stream().filter(value -> value%2 == 0).collect(Collectors.toList()));
     }
 }
